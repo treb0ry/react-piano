f73f886538522edf494b5bcf1a6aefa854ae55ed
treb0ry <vinodel2589@gmai.com>
2020-10-28 19:32:41 +0300

    added Note interface;MidiValue type;PitchIndex type

--- src/domain/note.ts
+++ src/domain/note.ts
@@ -1,3 +1,13 @@
 export type NoteType = "natural" | "flat" | "sharp";
 export type NotePitch = "A" | "B" | "C" | "D" | "E" | "F" | "G";
 export type OctaveIndex = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+export type MidiValue = number;
+export type PitchIndex = number;
+
+export interface Note {
+  midi: MidiValue;
+  type: NoteType;
+  pitch: NotePitch;
+  index: PitchIndex;
+  octave: OctaveIndex;
+}
