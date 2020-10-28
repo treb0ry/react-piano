2737bdea049031fa595e357920263888b5163cbd
treb0ry <vinodel2589@gmai.com>
2020-10-28 18:53:30 +0300

    added Footer,Logo to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-
-import './App.css';
+import React from "react";
+import { Footer } from "./components/Footer/Footer";
+import { Logo } from "./components/Logo/Logo";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <main className="app-content" />
+      <Footer />
     </div>
   );
 }
