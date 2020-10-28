b68cddad9a1cf1acfec9a6b90ce07dc48aee3582
treb0ry <vinodel2589@gmai.com>
2020-10-28 22:59:27 +0300

    changed Key and Keyboard props

--- src/components/Key/Key.tsx
+++ src/components/Key/Key.tsx
@@ -1,15 +1,23 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, ReactEventHandler } from "react";
 import clsx from "clsx";
 import { NoteType } from "../../domain/note";
 interface KeyProps {
   type: NoteType;
   label: string;
   disabled?: boolean;
+  onUp: ReactEventHandler<HTMLButtonElement>;
+  onDown: ReactEventHandler<HTMLButtonElement>;
 }
 export const Key: FunctionComponent<KeyProps> = (props) => {
-  const { type, label, ...rest } = props;
+  const { type, label, onDown, onUp, ...rest } = props;
   return (
-    <button className={clsx(`key key--${type}`)} type="button" {...rest}>
+    <button
+      className={clsx(`key key--${type}`)}
+      type="button"
+      onMouseDown={onDown}
+      onMouseUp={onUp}
+      {...rest}
+    >
       {label}
     </button>
   );
