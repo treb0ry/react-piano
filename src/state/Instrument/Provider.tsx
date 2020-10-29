1ac4d0e8c2192b5cfe1a6ce64ca43388931aa2c3
treb0ry <vinodel2589@gmai.com>
2020-10-29 18:02:19 +0300

    added InstrumentContextProvider

---
+++ src/state/Instrument/Provider.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent, useState } from "react";
+import { DEFAULT_INSTRUMENT } from "../../domain/sound";
+import { InstrumentContext } from "./InstrumentContext";
+
+export const InstrumentContextProvider: FunctionComponent = ({ children }) => {
+  const [instrument, setInstrument] = useState(DEFAULT_INSTRUMENT);
+  return (
+    <InstrumentContext.Provider value={{ instrument, setInstrument }}>
+      {children}
+    </InstrumentContext.Provider>
+  );
+};
