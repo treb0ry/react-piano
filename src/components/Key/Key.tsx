5bbc8d4e3d7371bef62912800d0430aa63380104
treb0ry <vinodel2589@gmai.com>
2020-10-29 20:35:04 +0300

    added color style to key component

--- src/components/Key/Key.tsx
+++ src/components/Key/Key.tsx
@@ -11,15 +11,23 @@ interface KeyProps {
   onDown: ReactEventHandler<HTMLButtonElement>;
 }
 export const Key: FunctionComponent<KeyProps> = (props) => {
+  const BackgroundColor = ["#7eb8da", "#92ddea", "#ffa5d8", "#be9ddf", "#9579d1"];
+  const color = BackgroundColor[Math.floor(Math.random() * BackgroundColor.length)];
+  const style = {
+    backgroundColor: `${color}`,
+    boxShadow: `0px 0px 20px 2px ${color}`,
+  };
   const { type, label, onDown, onUp, ...rest } = props;
   const pressed = usePressObserver({
     watchKey: label,
     onStartPress: onDown,
     onFinishPress: onUp,
   });
+  console.log(pressed);
   return (
     <button
       className={clsx(`key key--${type}`, pressed && "is-pressed")}
+      style={type === "natural" && pressed ? style : {}}
       type="button"
       onMouseDown={onDown}
       onMouseUp={onUp}
