4896dec0a261e931c28c1556d98c80ff3e803620
treb0ry <vinodel2589@gmai.com>
2020-10-29 18:26:33 +0300

    added Playground component

---
+++ src/components/Playground/Playground.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from "react";
+import { InstrumentContextProvider } from "../../state/Instrument/Provider";
+import { InstrumentSelector } from "../InstrumentSelector/InstrumentSelector";
+import { KeyboardWithInstrument } from "../Keyboard/WithInstrument";
+
+export const Playground: FunctionComponent = () => {
+  return (
+    <InstrumentContextProvider>
+      <div className="playground">
+        <KeyboardWithInstrument />
+        <InstrumentSelector />
+      </div>
+    </InstrumentContextProvider>
+  );
+};
